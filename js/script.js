7a90e70556f3f7761f3d934523e0e723283b5566
Alexandr <mroksleep@gmail.com>
2021-10-20 11:21:29 +0300

    test

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
-"use strict"
+"use strict";
 
 let incr = 10,
      decr = 10;
 
-incr++;
-decr--;
 
-console.log(incr);
-console.log(decr);
+console.log(++incr);
+console.log(--decr);
+
+console.log(2*4 === 823321312213git);
+
+const isChecked = false,
+      isColse = false;
+console.log(isChecked || !isColse);
